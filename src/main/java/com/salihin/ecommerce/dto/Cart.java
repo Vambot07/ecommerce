cc0c3484c61e4d2b36ee6178db922628ddb11723
Muhammad Salihin Bin Azizizol <SSVSBAMU@slake.sssb.com>
2026-05-04 17:33:36 +0800

    Add: redis features

---
+++ src/main/java/com/salihin/ecommerce/dto/Cart.java
@@ -0,0 +1,8 @@
+package com.salihin.ecommerce.dto;
+
+public class Cart {
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+
+}
---
+++ src/main/java/com/salihin/ecommerce/dto/CartItem.java
@@ -0,0 +1,4 @@
+package com.salihin.ecommerce.dto;
+
+public class CartItem {
+}
